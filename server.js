4735821db5f33dfdd0557ba9f12aa4919dbc2001
david millan <damillano93@gmail.com>
2019-02-27 20:06:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 process.env.NODE_ENV = 'development';
 app.get('/', function (req, res) {
-  res.send('hola mddundo como estamos!');
+  res.send('hola mundo automatizando todo!');
 });
 
 app.listen(3000, function () {
